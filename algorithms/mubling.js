aa5a48ce4aa4fa5295122fa594a634f9805259d6
Hannah Mitchell <hannah@Hannahs-MacBook-Pro-2.local>
2018-11-06 16:42:22 -0800

    added mubling algorithm

---
+++ algorithms/mubling.js
@@ -0,0 +1,4 @@
+// Write a funtion that produces the following output:
+// accum("abcd");    // "A-Bb-Ccc-Dddd"
+// accum("RqaEzty"); // "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+// accum("cwAt");    // "C-Ww-Aaa-Tttt"
