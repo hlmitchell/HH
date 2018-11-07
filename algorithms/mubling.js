3dfe23669522aed8d6a71b072a33e7d1f8a2f756
Hannah Mitchell <hannah@Hannahs-MacBook-Pro-2.local>
2018-11-06 16:45:48 -0800

    finished mumbling function

--- algorithms/mubling.js
+++ algorithms/mubling.js
@@ -2,3 +2,11 @@
 // accum("abcd");    // "A-Bb-Ccc-Dddd"
 // accum("RqaEzty"); // "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
 // accum("cwAt");    // "C-Ww-Aaa-Tttt"
+
+function accum(s) {
+	return s.split('').map((next, i, arr) => {
+    return next.toUpperCase() + next.toLowerCase().repeat(i);
+  }, '').join('-');
+}
+
+module.exports = accum;
