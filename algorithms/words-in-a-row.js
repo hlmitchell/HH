c6b8abb1932fd30ae76ba5bceb6eba4330cad96a
Hannah Mitchell <hannah@Hannahs-MacBook-Pro-2.local>
2018-11-14 16:02:11 -0800

    instructions

---
+++ algorithms/words-in-a-row.js
@@ -0,0 +1,20 @@
+// you will be given an array of keys that are unique
+// ['hi', 'bye' 'sup']
+// you will also be given an array of many words that contain the key words too
+// ['taco', 'hi', 'sup', 'bye', 'hi', 'taco']
+// we want to find all occurances in the above array in which
+// all the key words are present in a row, in any order
+// return an array that contains the indices of the start of every
+// instance
+
+// const keys = ['hi', 'bye' 'sup']
+// const words = ['taco', 'hi', 'sup', 'bye', 'hi', 'taco']
+// console.log(findWordsInARow(keys, words)); // [1,2]
+
+function findWordsInARow(keys, words) {
+
+}
+
+const keys = ['hi', 'bye' 'sup']
+const words = ['taco', 'hi', 'sup', 'bye', 'hi', 'taco']
+console.log(findWordsInARow(keys, words)); // [1,2]
