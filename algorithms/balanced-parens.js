2246419d917ade62e6d558ea7880985250f3fe19
Hannah Mitchell <hannah@Hannahs-MacBook-Pro-2.local>
2018-11-15 20:48:33 -0800

    refactored with object to store parens instead of using switch

--- algorithms/balanced-parens.js
+++ algorithms/balanced-parens.js
@@ -24,26 +24,46 @@
  *
  */
 
+// function balancedParens(input){
+//   const stack = [];
+//   for (let i = 0; i < input.length; i++) {
+//     switch(input[i]) {
+//       case '{':
+//       case '[':
+//       case '(':
+//         stack.push(input[i]);
+//         break;
+//       case '}':
+//         if (stack.pop() !== '{') return false;
+//         break;
+//       case ']':
+//         if (stack.pop() !== '[') return false;
+//         break;
+//       case ')':
+//         if (stack.pop() !== '(') return false;
+//         break;
+//       default:
+//         break;
+//     }
+//   }
+//   return stack.length > 0 ? false : true;
+// }
+
 function balancedParens(input){
+  // declare vars
+  const matches = {
+    '{' : '}',
+    '[' : ']',
+    '(' : ')'
+  }
   const stack = [];
-  for (let i = 0; i < input.length; i++) {
-    switch(input[i]) {
-      case '{':
-      case '[':
-      case '(':
-        stack.push(input[i]);
-        break;
-      case '}':
-        if (stack.pop() !== '{') return false;
-        break;
-      case ']':
-        if (stack.pop() !== '[') return false;
-        break;
-      case ')':
-        if (stack.pop() !== '(') return false;
-        break;
-      default:
-        break;
+  // iterate through our string
+  for (let i = 0; i < input.length; i ++) {
+    // encouter an opening parens, add it to our stack
+    if (matches.hasOwnProperty(input[i])) stack.push(input[i]);
+    // encouter a closing parens, we will pop off the stack and compare
+    else if (input[i] === ']' || input[i] === '}' || input[i] === ')') {
+      if (matches[stack.pop()] !== input[i]) return false;
     }
   }
   return stack.length > 0 ? false : true;
