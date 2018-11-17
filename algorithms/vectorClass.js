6dde98f481bd1e3dfdd266d5f49926f3858fbf70
Hannah Mitchell <hannah@Hannahs-MacBook-Pro-2.local>
2018-11-17 10:04:45 -0800

    instructions added

---
+++ algorithms/vectorClass.js
@@ -0,0 +1,12 @@
+// Create a Vector object that supports addition, subtraction, 
+// dot products, and norms. So, for example:
+
+// var a = new Vector([1, 2, 3]);
+// var b = new Vector([3, 4, 5]);
+// var c = new Vector([5, 6, 7, 8]);
+
+// a.add(b);      // should return a new Vector([4, 6, 8])
+// a.subtract(b); // should return a new Vector([-2, -2, -2])
+// a.dot(b);      // should return 1*3 + 2*4 + 3*5 = 26
+// a.norm();      // should return sqrt(1^2 + 2^2 + 3^2) = sqrt(14)
+// a.add(c);      // throws an error
