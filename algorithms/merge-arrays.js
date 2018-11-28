c0402e57362f2fff2457435b5143204c4a7c68b7
Hannah Mitchell <hannah@Hannahs-MacBook-Pro-2.local>
2018-11-28 12:57:43 -0800

    solution finished

--- algorithms/merge-arrays.js
+++ algorithms/merge-arrays.js
@@ -13,12 +13,27 @@
  *
  */
 
-function mergeArrays() {
+function mergeArrays(arr1, arr2) {
+  if (!Array.isArray(arr1) || !Array.isArray(arr2)) return;
+  const merged = [];
+  let i = 0;
+  let j = 0;
+  while (i < arr1.length || j < arr2.length) {
+    if (i >= arr1.length || arr2[j] < arr1[i]) {
+      merged.push(arr2[j]);
+      j += 1;
+    }
+    else if (j >= arr2.length || arr1[i] < arr2[j]) {
+      merged.push(arr1[i]);
+      i += 1;
+    }
+  }
 
+  return merged;
 }
 
 const my_array = [3,4,6,10,11,15,21];
 const another_array = [1,5,8,12,14,19];
-mergeArrays(my_array, another_array); // [1, 3, 4, 5, 6, 8, 10, 11, 12, 14, 15, 19, 21]
+console.log(mergeArrays(my_array, another_array)); // [1, 3, 4, 5, 6, 8, 10, 11, 12, 14, 15, 19, 21]
 
 module.exports = mergeArrays;
