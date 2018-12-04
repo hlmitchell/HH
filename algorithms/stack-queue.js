700c04affeb341c3ce31f898abc0ddb702369456
Hannah Mitchell <hannah@Hannahs-MacBook-Pro-2.local>
2018-12-03 19:50:09 -0800

    created queue using stacks

--- algorithms/stack-queue.js
+++ algorithms/stack-queue.js
@@ -1,3 +1,18 @@
 /**
  * Create a stack.Then create a queue using two stacks.
  */
+
+
+function Stack() {
+  const vals = {};
+  return {
+    length: 0,
+    push: function(val) {
+      vals[this.length++] = val;
+    },
+    pop: function() {
+      if (this.length === 0) return -1;
+      else return vals[--this.length];
+    }
+  }
+}
