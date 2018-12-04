2c36b6d020eef148553e7eb1a8c7c2ac78d14c97
Hannah Mitchell <hannah@Hannahs-MacBook-Pro-2.local>
2018-12-03 19:51:19 -0800

    tests added

--- algorithms/stack-queue.js
+++ algorithms/stack-queue.js
@@ -16,3 +16,41 @@ function Stack() {
     }
   }
 }
+
+function Queue() {
+  const firstStack = Stack();
+  const secondStack = Stack();
+  let length = 0;
+  return {
+    enqueue: function(val) {
+      if (secondStack.length > 0) {
+        for (let i = 0; i < length; i++) {
+          firstStack.push(secondStack.pop());
+        }
+      }
+      length += 1;
+      firstStack.push(val);
+    },
+    dequeue: function() {
+      if (firstStack.length > 0) {
+        for (let i = 0; i < length; i++) {
+          secondStack.push(firstStack.pop());
+        }
+      }
+      length -= 1;
+      return secondStack.pop();
+    }
+  }
+}
+
+const nq = Queue();
+
+nq.enqueue(1);
+console.log(nq.dequeue());
+nq.enqueue(2);
+nq.enqueue(3);
+
+console.log(nq.dequeue());
+console.log(nq.dequeue());
+
+module.exports = {Stack: Stack, Queue: Queue};
