9ec4dea980624ed3a65338cafd38d2df9f384a1e
Hannah Mitchell <hannah@hannahs-mbp-2.home>
2018-12-06 15:12:39 -0800

    instructions added

---
+++ algorithms/shuffle-songs.js
@@ -0,0 +1,12 @@
+/* Code an audio player that has a shuffle mode.
+
+Restrictions:
+  You can't shuffle the songs prior and then just iterate over them.
+  
+  The next song must be chosen at the time the current song ends.
+  
+  You shouldn't repeat a song until all songs have been played once.
+  
+  You can just to print the song to consider it "played"
+
+ */
