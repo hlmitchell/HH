a23405dcaf3efb83525e0385c872d158e91f26e7
Hannah Mitchell <hannah@Hannahs-MacBook-Pro-2.local>
2018-12-05 17:09:06 -0800

    solved using math

--- algorithms/knight-jumps.js
+++ algorithms/knight-jumps.js
@@ -13,11 +13,11 @@
 /*
 Observe this grid showing all the answers. There are two observations:
 1. The symmetry. (8 8) is the same as (1 1), (2 7) is the same as (2 2), etc. There is symmetry is both x and y axes.
-   We can therefore map everything to only the first quadrant. We'll right a "reflect" function to do that.
+  We can therefore map everything to only the first quadrant. We'll right a "reflect" function to do that.
 2. There is an island of 8's in the middle. The distance from this island determines the number of moves to deduct from 8.
-   We can calculate this distance and pass it through a function "distanceToValue" that determines the appropriate number of moves
-   to deduct based on the distance.
-   
+  We can calculate this distance and pass it through a function "distanceToValue" that determines the appropriate number of moves
+  to deduct based on the distance.
+  
 2 3 4 4 4 4 3 2
 3 4 6 6 6 6 4 3
 4 6 8 8 8 8 6 4
@@ -27,6 +27,16 @@ Observe this grid showing all the answers. There are two observations:
 3 4 6 6 6 6 4 3
 2 3 4 4 4 4 3 2
 */
+
+function knightjumps(str) {
+  var x = reflect(+str[1]); // yet another way to parse integers, then reflect the coordinate
+  var y = reflect(+str[3]);
+  var x_distance = Math.max(3 - x, 0);
+  var y_distance = Math.max(3 - y, 0);
+  return 8 - distanceToValue(x_distance + y_distance);
+}
+function distanceToValue(d) { return d < 2 ? 2 * d : d + 2; }
+function reflect(n) { return n > 4 ? 9 - n : n; }
 
 // function knightjumps(str) {
 //   // edge cases
