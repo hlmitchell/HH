4c636fabe2f9098012bd472cb88fd26a17995b84
Hannah Mitchell <hannah@Hannahs-MacBook-Pro-2.local>
2018-12-05 17:08:15 -0800

    added relative instructions for better approach

--- algorithms/knight-jumps.js
+++ algorithms/knight-jumps.js
@@ -10,36 +10,54 @@
 //  example input:
 // var str = "(4 5)"
 
-function knightjumps(str) {
-  // edge cases
-  if (typeof str !== 'string' || str.length < 5 || str.length > 5) return 0;
-  const x = parseInt(str[1]);
-  if (isNaN(x) || x < 1 || x > 8) return 0;
-  const y = parseInt(str[3]);
-  if (isNaN(y) || y < 1 || y > 8) return 0;
+/*
+Observe this grid showing all the answers. There are two observations:
+1. The symmetry. (8 8) is the same as (1 1), (2 7) is the same as (2 2), etc. There is symmetry is both x and y axes.
+   We can therefore map everything to only the first quadrant. We'll right a "reflect" function to do that.
+2. There is an island of 8's in the middle. The distance from this island determines the number of moves to deduct from 8.
+   We can calculate this distance and pass it through a function "distanceToValue" that determines the appropriate number of moves
+   to deduct based on the distance.
+   
+2 3 4 4 4 4 3 2
+3 4 6 6 6 6 4 3
+4 6 8 8 8 8 6 4
+4 6 8 8 8 8 6 4
+4 6 8 8 8 8 6 4
+4 6 8 8 8 8 6 4
+3 4 6 6 6 6 4 3
+2 3 4 4 4 4 3 2
+*/
 
-  let count = 0;
-  // left and right movement
-  if (x + 2 <= 8) {
-    if (y + 1 <= 8) count += 1;
-    if (y - 1 > 0) count += 1;
-  }
-  if (x - 2 > 0) {
-    if (y + 1 <= 8) count += 1;
-    if (y - 1 > 0) count += 1;
-  }
+// function knightjumps(str) {
+//   // edge cases
+//   if (typeof str !== 'string' || str.length < 5 || str.length > 5) return 0;
+//   const x = parseInt(str[1]);
+//   if (isNaN(x) || x < 1 || x > 8) return 0;
+//   const y = parseInt(str[3]);
+//   if (isNaN(y) || y < 1 || y > 8) return 0;
 
-  // upward and downward movement
-  if (y + 2 <= 8) {
-    if (x + 1 <= 8) count += 1;
-    if (x - 1 > 0) count += 1;
-  }
-  if (y - 2 > 0) {
-    if (x + 1 <= 8) count += 1;
-    if (x - 1 > 0) count += 1;
-  }
+//   let count = 0;
+//   // left and right movement
+//   if (x + 2 <= 8) {
+//     if (y + 1 <= 8) count += 1;
+//     if (y - 1 > 0) count += 1;
+//   }
+//   if (x - 2 > 0) {
+//     if (y + 1 <= 8) count += 1;
+//     if (y - 1 > 0) count += 1;
+//   }
 
-  return count;
-}
+//   // upward and downward movement
+//   if (y + 2 <= 8) {
+//     if (x + 1 <= 8) count += 1;
+//     if (x - 1 > 0) count += 1;
+//   }
+//   if (y - 2 > 0) {
+//     if (x + 1 <= 8) count += 1;
+//     if (x - 1 > 0) count += 1;
+//   }
+
+//   return count;
+// }
 
 module.exports = knightjumps;
