f068f6422198001f0e1352f2cd83031a3caf812c
Hannah Mitchell <hannah@hannahs-mbp-2.home>
2018-12-06 16:11:45 -0800

    finished

--- algorithms/shuffle-songs.js
+++ algorithms/shuffle-songs.js
@@ -10,3 +10,29 @@ Restrictions:
   You can just to print the song to consider it "played"
 
  */
+
+const playList = ['Bohemian Rhapsody', 'Comfortably Numb', 'Piano Man', 'Hotel California', 'Cocaine']
+
+function shuffle(songs) {
+  let dummy = 0;
+  let unplayed = songs;
+  let played = [];
+
+  const interval = setInterval(() => {
+
+    const i = Math.floor(Math.random() * unplayed.length);
+    console.log(unplayed[i]);
+    played.push(unplayed[i]);
+    unplayed.splice(i, 1);
+    if (unplayed.length === 0) {
+      unplayed = played;
+      played = [];
+    }
+
+    // stop playing song after iterations
+    dummy += 1;
+    if (dummy > 10) clearInterval(interval);
+  }, 1000);
+}
+
+shuffle(playList);
