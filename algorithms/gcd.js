33896a312f485aa058e069c690e1a1112dd61189
Hannah Mitchell <hannah@Hannahs-MacBook-Pro-2.local>
2018-12-17 08:09:25 -0800

    first solution done

--- algorithms/gcd.js
+++ algorithms/gcd.js
@@ -8,7 +8,13 @@
  */
 
 function gcd(a, b) {
+  if (isNaN(a) || isNaN(b)) return;
+  let min = Math.min(a, b);
+  for (let i = min; i > 1; i--) {
+    if (a % i === 0 && b % i === 0) return i;
+  }
 
+  return 1;
 }
 
 module.exports = gcd;
