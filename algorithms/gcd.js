3bf4bd24e3461ed634f1c0e35b1218e437f2fd5d
Hannah Mitchell <hannah@Hannahs-MacBook-Pro-2.local>
2018-12-17 08:13:18 -0800

    refactored to be more efficient

--- algorithms/gcd.js
+++ algorithms/gcd.js
@@ -10,7 +10,10 @@
 function gcd(a, b) {
   if (isNaN(a) || isNaN(b)) return;
   let min = Math.min(a, b);
-  for (let i = min; i > 1; i--) {
+  let max = min === a ? b : a;
+  if (max % min === 0) return min;
+
+  for (let i = Math.ceil(min / 2); i > 1; i--) {
     if (a % i === 0 && b % i === 0) return i;
   }
 
