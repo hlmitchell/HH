2f85166e8e99023465e2ed01a8f3bd69a2379586
Hannah Mitchell <hannah@Hannahs-MacBook-Pro-2.local>
2018-12-28 14:28:44 -0800

    expected output added

--- algorithms/merge-ranges.js
+++ algorithms/merge-ranges.js
@@ -12,3 +12,17 @@
 
 // Write a function mergeRanges() that takes an array of multiple meeting time ranges and returns 
 // an array of condensed ranges.
+
+const input = [
+  { startTime: 0,  endTime: 1 },
+  { startTime: 3,  endTime: 5 },
+  { startTime: 4,  endTime: 8 },
+  { startTime: 10, endTime: 12 },
+  { startTime: 9,  endTime: 10 },
+]
+
+// output =>   [
+//   { startTime: 0, endTime: 1 },
+//   { startTime: 3, endTime: 8 },
+//   { startTime: 9, endTime: 12 },
+// ]
