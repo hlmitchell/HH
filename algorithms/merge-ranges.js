ea59dd7cdac966047687fd0ad9a1be704f129a49
Hannah Mitchell <hannah@Hannahs-MacBook-Pro-2.local>
2018-12-28 15:08:40 -0800

    solved

--- algorithms/merge-ranges.js
+++ algorithms/merge-ranges.js
@@ -15,6 +15,21 @@
 
 // Do not assume the meetings are in order. The meeting times are coming from multiple teams.
 
+function mergeRanges(meetings) {
+  const sorted = meetings.sort((a, b) => a.startTime > b.startTime);
+  const output = [sorted[0]];
+
+  for (let i = 1; i < sorted.length; i++) {
+    const tracker = output.length - 1;
+    if (output[tracker].endTime >= sorted[i].startTime) {
+      if (output[tracker].endTime < sorted[i].endTime) output[tracker].endTime = sorted[i].endTime;
+    }
+    else output.push(sorted[i]);
+  }
+
+  return output;
+}
+
 const input = [
   { startTime: 0,  endTime: 1 },
   { startTime: 3,  endTime: 5 },
@@ -23,8 +38,22 @@ const input = [
   { startTime: 9,  endTime: 10 },
 ]
 
+// console.log(mergeRanges(input));
+
 // output =>   [
 //   { startTime: 0, endTime: 1 },
 //   { startTime: 3, endTime: 8 },
 //   { startTime: 9, endTime: 12 },
 // ]
+
+const input2 =   [
+  { startTime: 1, endTime: 10 },
+  { startTime: 2, endTime: 6 },
+  { startTime: 3, endTime: 5 },
+  { startTime: 7, endTime: 9 },
+]
+
+console.log(mergeRanges(input2));
+
+// output => [ { startTime: 1, endTime: 10 } ]
+
