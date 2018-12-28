b1396399415aec17f6538162a08b78f091fd2468
Hannah Mitchell <hannah@Hannahs-MacBook-Pro-2.local>
2018-12-28 15:16:36 -0800

    finished

--- algorithms/reverse-array.js
+++ algorithms/reverse-array.js
@@ -1,7 +1,22 @@
 // Write a function that takes an array of characters and reverses the letters in place.
 
-function reverseArray() {
+function reverseArray(arr) {
+  if (!Array.isArray(arr)) return arr;
 
+  let front = 0;
+  let back = arr.length - 1;
+  let temp;
+
+  while (front < back) {
+    temp = arr[front];
+    arr[front] = arr[back];
+    arr[back] = temp;
+    
+    front += 1;
+    back -= 1;
+  }
+
+  return arr;
 }
 
 console.log(reverseArray([1,2,3,4,5]));
