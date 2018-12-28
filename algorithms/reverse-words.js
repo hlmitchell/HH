d6e0d17a14efa794900656b50f8ee8c0c41468fc
Hannah Mitchell <hannah@Hannahs-MacBook-Pro-2.local>
2018-12-28 15:44:15 -0800

    solution

--- algorithms/reverse-words.js
+++ algorithms/reverse-words.js
@@ -5,8 +5,20 @@
 // Write a function reverseWords() that takes a message as an array of characters and reverses the order 
 // of the words in place. ↴
 
+const reverseArray = require('./reverse-array');
+
 function reverseWords(message) {
-  
+  reverseArray(message);
+
+  let front = 0;
+  for (let i = 0; i < message.length; i++) {
+    if (message[i] === ' ') {
+      reverseArray(message, front, i - 1)
+      front = i + 1;
+    } else if (i + 1 === message.length) {
+      reverseArray(message, front, i)
+    }
+  }
 }
 
 const message = [ 'c', 'a', 'k', 'e', ' ',
@@ -14,5 +26,5 @@ const message = [ 'c', 'a', 'k', 'e', ' ',
                 's', 't', 'e', 'a', 'l' ];
 
 reverseWords(message);
-console.log(message.join());
+console.log(message.join(''));
 // Prints: 'steal pound cake'
