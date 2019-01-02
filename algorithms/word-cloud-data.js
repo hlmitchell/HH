1281d3b13f20b2b0808957b46437b8e200a6d83e
Hannah Mitchell <hannah@Hannahs-MacBook-Pro-2.local>
2019-01-02 12:27:51 -0800

    first solution

--- algorithms/word-cloud-data.js
+++ algorithms/word-cloud-data.js
@@ -16,3 +16,30 @@
 // include one "Add" or "add" with a value of 22. Make reasonable (not necessarily perfect) decisions about 
 // cases like "After" and "Dana".
 
+function wordCloud(sentence) {
+  const punc = new Set();
+  punc.add(',');
+  punc.add('.');
+  punc.add(':');
+  punc.add(' ');
+  punc.add('');
+
+  const frequency = {};
+
+  let tempWord = '';
+
+  for (let i = 0; i < sentence.length; i++) {
+    if (!punc.has(sentence[i])) tempWord += sentence[i].toLowerCase();
+    else if (tempWord !== '') {
+      if (!frequency[tempWord]) frequency[tempWord] = 1;
+      else frequency[tempWord] += 1;
+      tempWord = '';
+    }
+  }
+
+  return frequency;
+}
+
+const sentence = "After beating the eggs, Dana read the next step: Add milk and eggs, then add flour and sugar.";
+
+console.log(wordCloud(sentence));
