a3199faee7f220eb8b99451cb75293fbb832ca51
Hannah Mitchell <hannah@hannahs-mbp-2.home>
2019-01-05 20:07:45 -0800

    note

--- algorithms/highest-of-three.js
+++ algorithms/highest-of-three.js
@@ -8,6 +8,8 @@ function highestOfThree(integers) {
   const potentialSum1 = integers[integers.length - 1] * integers[integers.length - 2] * integers[integers.length - 3];
   const potentialSum2 = integers[0] * integers[1] * integers[integers.length - 1];
   return Math.max(potentialSum1, potentialSum2);
+
+  // could solve in O(n) time if sort is ommited and a lot more lines of code are added
 }
 
 const integers = [8,2,6,5,7,3,-10,-10];
