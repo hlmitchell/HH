1b56c27aad9f0bb55ca6d4ecf55e25de611c929c
Hannah Mitchell <hannah@hannahs-mbp-2.home>
2019-01-05 19:04:39 -0800

    instructions

---
+++ algorithms/highest-of-three.js
@@ -0,0 +1,7 @@
+// Given an array of integers, find the highest product you can get from three of the integers.
+
+// The input arrayOfInts will always have at least three integers.
+
+const integers = [8,2,6,5,7,3];
+
+console.log(highestOfThree(integers));
