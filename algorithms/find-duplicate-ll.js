124e24ac19c8a96b5d97ca713b573732afc355be
Hannah Mitchell <hannah@Hannahs-MacBook-Pro-2.local>
2019-01-16 16:15:10 -0800

    solved in O(n) space and time

--- algorithms/find-duplicate-ll.js
+++ algorithms/find-duplicate-ll.js
@@ -13,9 +13,28 @@ head.next.next.next.next = new Node(5);
 head.next.next.next.next.next = head.next.next;
 
 function findCycleRoot(node) {
+  // modifiers of nodes to keep track
+  node.tracker1 = true;
 
+  // tracker2 will increment every loop, tracker1 will increment every other loop
+  let tracker1 = node;
+  let tracker2 = node;
 
+  // determines which trackers should move
+  let counter = 1;
 
+  // not a cycle if loop terminates
+  while(tracker2.next !== null) {
+    if (counter % 2 === 0) {
+      tracker1 = tracker1.next;
+      tracker1.tracker1 = true;
+    }
+
+    tracker2 = tracker2.next;
+    if (tracker2.tracker1) return tracker2.value;
+
+    counter += 1;
+  }
 }
 
 console.log(findCycleRoot(head));
