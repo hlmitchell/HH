7c58bfb48017cc99bf1050f7a15f4d4026372e31
Hannah Mitchell <hannah@Hannahs-MacBook-Pro-2.local>
2019-01-18 17:15:13 -0800

    solved by counting number of times a character appears

--- algorithms/perm-palin.js
+++ algorithms/perm-palin.js
@@ -10,7 +10,21 @@
  */
 
 function permPalin(str) {
-	
+  // we will keep track of how many times every letter appears
+  count = {};
+
+  // loop through the string to count the letters
+  for (let i = 0; i < str.length; i++) {
+    if (!count[str[i]]) count[str[i]] = 1;
+    else count[str[i]] += 1;
+  }
+  // if only a single letter (or none) has an odd number of occurences, it can be a palindrome
+  return Object.values(count).filter(num => num % 2 === 1).length <= 1;
 }
+
+console.log(permPalin('abab'))// => true
+console.log(permPalin('cbaba'))// => true
+console.log(permPalin('cbac'))// => false
+console.log(permPalin('a'))// => true
 
 module.exports = permPalin;
