cf4d4c452bbc1f909e01b300767b7ee706746e38
Hannah Mitchell <hannah@hannahs-mbp-2.home>
2019-01-29 13:37:27 -0800

    lists will be merged two at a time and then a recursive call will be made

--- algorithms/merge-k-sorted-lists.js
+++ algorithms/merge-k-sorted-lists.js
@@ -8,8 +8,27 @@
 // ]
 // Output: 1->1->2->3->4->4->5->6
 
-function mergeLists() {
-
+const merge2Lists = function(list1 ,list2) {
+    
 }
+
+const mergeKLists = function(lists) {
+    // edge case for no lists
+    if (lists.length < 1) return;
+    // recursive base case
+    if (lists.length === 1) return lists[0];
+    
+    counter = 0;
+    const newLists = [];
+    
+    while (counter < lists.length - 2) {
+        newLists.push(merge2Lists(lists[counter], lists[counter + 1]));
+        counter += 2;
+    }
+    // add the final list if there was an odd number
+    if (lists.length % 2 === 1) newLists.push(lists[lists.length - 1]);
+    
+    console.log(newLists);
+};
 
 module.exports = mergeLists;
