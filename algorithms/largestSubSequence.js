3cd47a8ef525fb22becb120c2a36e3459357ee86
Hannah Mitchell <hannah@hannahs-mbp-2.home>
2019-02-08 11:25:28 -0800

    solved in linear time

--- algorithms/largestSubSequence.js
+++ algorithms/largestSubSequence.js
@@ -5,7 +5,24 @@
 const sequence = [10,3,7,9,0,15];
 
 function largestSubSequence(arr) {
+  let start, end, potentialStart, potentialEnd, length, potentialLength;
+  start = end = potentialStart = potentialEnd = length = potentialLength = 0;
 
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > arr[i - 1]) {
+      potentialEnd = i;
+      potentialLength += 1;
+      potentialStart = potentialEnd - potentialLength;
+    } else if (potentialLength > length) {
+        start = potentialStart;
+        end = potentialEnd;
+        length = potentialLength;
+
+        potentialStart = potentialEnd = potentialLength = 0;
+    }
+  }
+
+  return [start, end];
 }
 
 console.log(largestSubSequence(sequence));
