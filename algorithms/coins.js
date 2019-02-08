bef9594a2294e441c0d21503b562b426a3fb2774
Hannah Mitchell <hannah@hannahs-mbp-2.home>
2019-02-08 09:09:36 -0800

    clarified instructions

--- algorithms/coins.js
+++ algorithms/coins.js
@@ -8,7 +8,7 @@
 // an array of coin denominations
 // computes the number of ways to make the amount of money with coins of the available denominations.
 
-// Example: for amount=44 (44¢) and denominations=[1,2,3][1,2,3] (11¢, 22¢ and 33¢), your program would 
+// Example: for amount=44 (44¢) and denominations=[1,2,3] (1¢, 2¢ and 3¢), your program would 
 // output 44—the number of ways to make 44¢ with those denominations:
 
 function coinsum() {
