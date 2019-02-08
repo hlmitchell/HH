4b3c76287e7e7eb107dbc1fd550ff48a7c3a0da9
Hannah Mitchell <hannah@hannahs-mbp-2.home>
2019-02-08 11:25:14 -0800

    instructions

---
+++ algorithms/largestSubSequence.js
@@ -0,0 +1,11 @@
+// Given an array of integers. Find the largest increasing sub sequence of integers in the array.
+// 10, 3, 7, 9, 0, 15
+// return index 1&3
+
+const sequence = [10,3,7,9,0,15];
+
+function largestSubSequence(arr) {
+
+}
+
+console.log(largestSubSequence(sequence));
